c2a35df92401035c3758124adfb5daded8a31958
Anthony Devine <antdevine@hotmail.co.uk>
2022-08-14 22:31:03 +0100

    started adding rating component

---
+++ components/RatingSelection.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>How did we do?</h1>
+        <p>Please let us know how we did with your support request.</p>
+        <form>
+            <input type="radio" id="html" name="fav_language" value="HTML">
+            <label for="html">HTML</label><br>
+            <input type="radio" id="css" name="fav_language" value="CSS">
+            <label for="css">CSS</label><br>
+            <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+            <label for="javascript">JavaScript</label>
+        </form>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
---
+++ pages/rating-selection.vue
@@ -0,0 +1,11 @@
+<template>
+  <RatingSelection/>
+</template>
+
+<script>
+import RatingSelection from '../components/RatingSelection.vue';
+export default {
+    name: "RatingSelectionPage",
+    components: { RatingSelection }
+}
+</script>
